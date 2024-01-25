645e27585c7b1482547147905b2ee2815e0ba515
Vamblack-DMV <vamblack2000@gmail.com>
2024-01-25 12:37:44 -0300

    Json Server Ativado inicio JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+const searchInput = document.getElementById ("search-input");
+const resultsArtist = document.getElementById ("result-artist");
+const resultPlaylist = document.getElementById ("result-playlists")
+
+document.addEventListener ("input", function() {
+    const searchTerm = searchInput.value.toLoweCase();
+    if (searchTerm === "") {
+
+    }
+} );
